e98880b9e7b32b1e5897bb0fbe871018e4a25963
PatrickIOb <patrick.bauer95@gmx.net>
2025-10-18 12:01:12 +0200

    serialization per animal put into a function

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,49 +1,61 @@
 import json
 
-# Read the JSON file
-with open("animals_data.json", "r") as json_file:
-    data = json.load(json_file)
-
-# Read the HTML template
-with open("animals_template.html", "r") as html_file:
-    template_content = html_file.read()
-
-# Build the output string
-output = ''  # define an empty string
-
-for animal_data in data:
+# Funktion zum Erstellen des HTML-Codes für ein einzelnes Tier
+def serialize_animal(animal_obj):
+    output = ''  # define an empty string
     output += '<li class="cards__item">\n'
 
-    # --- Title (Name) ---
-    if 'name' in animal_data:
-        output += f'  <div class="card__title">{animal_data["name"]}</div>\n'
+    # Name
+    if 'name' in animal_obj:
+        output += f'  <div class="card__title">{animal_obj["name"]}</div>\n'
 
-    # --- Text section ---
+    # Textblock starten
     output += '  <p class="card__text">\n'
 
-    # Characteristics might not exist
-    if 'characteristics' in animal_data:
-        characteristics = animal_data['characteristics']
+    # Characteristics (kann fehlen)
+    if 'characteristics' in animal_obj:
+        characteristics = animal_obj['characteristics']
 
         if 'diet' in characteristics:
             output += f'      <strong>Diet:</strong> {characteristics["diet"]}<br/>\n'
         if 'type' in characteristics:
             output += f'      <strong>Type:</strong> {characteristics["type"]}<br/>\n'
 
-    # Locations might not exist or be empty
-    if 'locations' in animal_data and animal_data['locations']:
-        locations = ", ".join(animal_data['locations'])
+    # Locations (kann fehlen oder leer sein)
+    if 'locations' in animal_obj and animal_obj['locations']:
+        locations = ", ".join(animal_obj['locations'])
         output += f'      <strong>Location:</strong> {locations}<br/>\n'
 
+    # Textblock und Listenelement schließen
     output += '  </p>\n'
-    output += '</li>\n\n'  # end of one card
+    output += '</li>\n\n'
+
+    return output
 
 
+# Hauptfunktion
+def main():
+    # JSON-Datei lesen
+    with open("animals_data.json", "r") as json_file:
+        data = json.load(json_file)
+
+    # HTML-Template lesen
+    with open("animals_template.html", "r") as html_file:
+        template_content = html_file.read()
+
+    # Ausgabe aufbauen
+    output = ''  # define an empty string
+    for animal_data in data:
+        output += serialize_animal(animal_data)
+
+    # Platzhalter ersetzen
+    new_html = template_content.replace("__REPLACE_ANIMALS_INFO__", output)
+
+    # Neue Datei schreiben
+    with open("animals.html", "w") as output_file:
+        output_file.write(new_html)
 
 
-# Replace the placeholder in the template
-new_html = template_content.replace("__REPLACE_ANIMALS_INFO__", output)
-
-# Write the new HTML content to a new file
-with open("animals.html", "w") as output_file:
-    output_file.write(new_html)
+# Programm starten
+if __name__ == "__main__":
+    main()

1c7ecf77b576a655081446618ead9b1623061d21
PatrickIOb <patrick.bauer95@gmx.net>
2025-10-18 11:26:46 +0200

    enhanced the animal cards

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -12,35 +12,33 @@ with open("animals_template.html", "r") as html_file:
 output = ''  # define an empty string
 
 for animal_data in data:
-    #html formatting
-    output += '<li class="cards__item">'
+    output += '<li class="cards__item">\n'
 
-    # Name
+    # --- Title (Name) ---
     if 'name' in animal_data:
-        output += f"Name: {animal_data['name']}<br/>\n"
+        output += f'  <div class="card__title">{animal_data["name"]}</div>\n'
+
+    # --- Text section ---
+    output += '  <p class="card__text">\n'
 
     # Characteristics might not exist
     if 'characteristics' in animal_data:
         characteristics = animal_data['characteristics']
 
         if 'diet' in characteristics:
-            output += f"Diet: {characteristics['diet']}<br/>\n"
+            output += f'      <strong>Diet:</strong> {characteristics["diet"]}<br/>\n'
         if 'type' in characteristics:
-            output += f"Type: {characteristics['type']}<br/>\n"
+            output += f'      <strong>Type:</strong> {characteristics["type"]}<br/>\n'
 
     # Locations might not exist or be empty
     if 'locations' in animal_data and animal_data['locations']:
         locations = ", ".join(animal_data['locations'])
-        output += f"Location: {locations}<br/>\n"
+        output += f'      <strong>Location:</strong> {locations}<br/>\n'
 
-    #end of list
-    output += '</li>'
-
-    # Add a blank line between animals
-    output += "\n"
+    output += '  </p>\n'
+    output += '</li>\n\n'  # end of one card
 
 
-    print(output)
 
 
 # Replace the placeholder in the template

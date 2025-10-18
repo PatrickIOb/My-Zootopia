b499877ffec316cd6531428c7ef7d309315b2f0a
PatrickIOb <patrick.bauer95@gmx.net>
2025-10-18 11:10:44 +0200

    bring animal data into html

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,22 +1,42 @@
 import json
 
-def load_data(file_path):
-  """ Loads a JSON file """
-  with open(file_path, "r") as handle:
-    return json.load(handle)
+# Read the JSON file
+with open("animals_data.json", "r") as json_file:
+    data = json.load(json_file)
 
-animals_data = load_data('animals_data.json')
+# Read the HTML template
+with open("animals_template.html", "r") as html_file:
+    template_content = html_file.read()
 
-def filter_animals(animals):
-    for animal in animals:
-        if "name" in animal:
-            print(f'Name: {animal["name"]}')
-        if "diet" in animal["characteristics"]:
-            print(f'Diet: {animal["characteristics"]["diet"]}')
-        if "locations" in animal:
-            print(f'Location: {animal["locations"][0]}')
-        if "type" in animal["characteristics"]:
-            print(f'Type: {animal["characteristics"]["type"]}')
-        print()
+# Build the output string
+output = ''  # define an empty string
 
-filter_animals(animals_data)
+for animal_data in data:
+    # Name
+    if 'name' in animal_data:
+        output += f"Name: {animal_data['name']}\n"
+
+    # Characteristics might not exist
+    if 'characteristics' in animal_data:
+        characteristics = animal_data['characteristics']
+
+        if 'diet' in characteristics:
+            output += f"Diet: {characteristics['diet']}\n"
+        if 'type' in characteristics:
+            output += f"Type: {characteristics['type']}\n"
+
+    # Locations might not exist or be empty
+    if 'locations' in animal_data and animal_data['locations']:
+        locations = ", ".join(animal_data['locations'])
+        output += f"Location: {locations}\n"
+
+    # Add a blank line between animals
+    output += "\n"
+
+
+# Replace the placeholder in the template
+new_html = template_content.replace("__REPLACE_ANIMALS_INFO__", output)
+
+# Write the new HTML content to a new file
+with open("animals.html", "w") as output_file:
+    output_file.write(new_html)
